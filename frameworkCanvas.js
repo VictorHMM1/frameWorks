0c878b62e1411a4adfd166848b47e6e4eb6046cd
Victor <victorm.ifpr2024@gmail.com>
2026-03-27 09:08:54 -0300

    Add files via upload

---
+++ frameworkCanvas.js
@@ -0,0 +1,23 @@
+const body = document.getElementByTagName("body")[0];
+const canvas = document.createElement("canvas");
+body.appendChild(canvas);
+const ctx = canvas.getContext("2d");
+const arco = document.querySelector("circulo");
+
+const circulo = {
+	x:100,
+	y:100,
+	r:50,
+	cor:"red",
+	desenhar : function (raio) {
+		ctx.beginPath();
+		ctx.arc(this.x,   this.y,   raio || this.r,   0,   2*Matn.PI);
+		ctx.fillStyle=this.cor || cor;
+		ctx.fill();
+		ctx.closePath();
+	}
+}
+if(arco){
+	let raio = arco.getAttribute("raio");
+	circulo.desenhar(raio);
+}
